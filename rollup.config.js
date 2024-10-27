9027d0b0cb3e3352acad417ab0d13440c8903e6e
Özenç Bilgili <18405305+ozencb@users.noreply.github.com>
2024-10-28 00:28:23 +0300

    Add rollup terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import typescript from 'rollup-plugin-typescript2';
-
+import { terser } from 'rollup-plugin-terser';
 export default {
   input: 'src/index.ts',
   output: [
@@ -13,5 +13,5 @@ export default {
       format: 'es',
     },
   ],
-  plugins: [typescript()],
+  plugins: [typescript(), terser()],
 };
