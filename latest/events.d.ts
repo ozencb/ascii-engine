6609992672c7beae7e396b0f1d916181df77edd4
ozencb <ozencb@users.noreply.github.com>
2024-10-27 21:17:33 +0000

    deploy: b34e17c1fafc6046f01b418c6e79dcccf63d2951

---
+++ latest/events.d.ts
@@ -0,0 +1,3 @@
+import { AnimationContext, CursorContext, RenderOptions } from './types';
+export declare const addPointerEvents: (target: HTMLElement, context: AnimationContext, cursor: CursorContext) => void;
+export declare const addWindowEvents: (target: HTMLElement, context: AnimationContext, options: RenderOptions) => void;
