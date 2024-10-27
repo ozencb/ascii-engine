3550ce74a84fe88379c92f40513689043678a0dc
ozencb <ozencb@users.noreply.github.com>
2024-10-27 20:37:15 +0000

    deploy: 9ccfb8d2f7a46e9e0654693be1d1ad69c3e9dcfd

---
+++ v1.0.0/constants.d.ts
@@ -0,0 +1,14 @@
+export declare enum Resolution {
+    Low = 1,
+    MediumLow = 2,
+    Medium = 3,
+    MediumHigh = 4,
+    High = 5,
+    VeryHigh = 6,
+    Ultra = 7,
+    UltraHigh = 8,
+    Extreme = 9,
+    Maximum = 10
+}
+export declare const Nbsp = "\u00A0";
+export declare const ScaleFactor = 0.05;
